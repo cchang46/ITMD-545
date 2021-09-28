6056c037c9de1959b17a41cb6736fecb097957e7
Chia-Chi Chang <cchang46@hawk.iit.edu>
2021-09-28 02:37:57 -0500

    Set up constraints and request media access

---
+++ public/javascripts/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const $self = {
+
+    constraints: { audio: false, video:true }
+
+};
+
+requestUserMedia($self.constraints);
+
+async function requestUserMedia(constraints) {
+   $self.stream = await navigator.mediaDevices.getUserMedia(constraints);
+}
