67486d0c0138ec7798f7954e487b7b76c7d02ca0
Chia-Chi Chang <cchang46@hawk.iit.edu>
2021-10-05 23:20:13 -0500

    Set up politeness and basic RTC callbacks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,18 +2,25 @@
 
 const $self = {
   rtcConfig: null,
-  constraints: { audio: false, video:true }
+  constraints: { audio: false, video: true },
+  isPolite: false,
+  isMakingOffer: false,
+  isIgnoringOffer: false,
+  isSettingRemoteAnswerPending: false
 };
 
 const $peer = {
   connection: new RTCPeerConnection($self.rtcConfig)
 };
 
+
+
 async function requestUserMedia(constraints) {
   const video = document.querySelector('#self');
   $self.stream = await navigator.mediaDevices.getUserMedia(constraints);
   video.srcObject = $self.stream;
   video.style.display = 'block';
+  return $self.stream
 }
 
 function stopUserMedia() {
@@ -37,7 +44,6 @@ const sc = io(`/${namespace}`, { autoConnect: false});
 
 registerScEvents();
 
-
 const button = document.querySelector('#call-yuuki');
 button.addEventListener('click', handleButton);
 /* DOM Events */
@@ -46,23 +52,58 @@ function handleButton(e) {
   if (button.className === 'join') {
     button.className = 'leave';
     button.innerText = 'Leave Chat';
-    requestUserMedia($self.constraints);
-    // joinChat();
+    requestUserMedia($self.constraints).then(() => joinChat());
   } else {
     button.className = 'join';
     button.innerText = 'Join Chat';
     stopUserMedia();
-    // leaveChat();
+    leaveChat();
   }
 }
 
 function joinChat() {
    sc.open();
+   registerRtcEvents($peer);
+   establishCallFeatures($peer);
 }
 
 function leaveChat() {
    sc.close();
 }
+
+/* WebRTC Events */
+function establishCallFeatures(peer) {
+  peer.connection.addTrack($self.stream.getTracks()[0],
+      $self.stream);
+}
+
+function registerRtcEvents(peer) {
+  peer.connection
+    .onnegotiationneeded = handleRtcNegotiation;
+  peer.connection
+    .onicecandidate = handleIceCandidate;
+  peer.connection
+    .ontrack = handleRtcTrack;
+}
+
+async function handleRtcNegotiation() {
+  console.log('RTC negotiation needed...');
+  // send an SDP description
+  $self.isMakingOffer = true;
+  await $peer.connection.setLocalDescription();
+  sc.emit('signal', { description:
+    $peer.connection.localDescription });
+  $self.isMakingOffer = false;
+}
+function handleIceCandidate({ candidate }) {
+  // send ICE candidate
+  sc.emit('signal', { candidate:
+    candidate });
+}
+function handleRtcTrack() {
+  // attach our track to the DOM somehow
+}
+
 
 /* Signaling Channel Events */
 
@@ -70,7 +111,7 @@ function registerScEvents() {
   sc.on('connect', handleScConnect);
   sc.on('connected peer', handleScConnectedPeer);
   sc.on('signal', handleScSignal);
-  sc.on('disconnected peer', handleScDisconnectedPeer)
+  sc.on('disconnected peer', handleScDisconnectedPeer);
 }
 
 
@@ -79,12 +120,18 @@ function handleScConnect() {
 }
 function handleScConnectedPeer() {
   console.log('Heard connected peer event!');
+  $self.isPolite = true;
 }
 function handleScDisconnectedPeer() {
   console.log('Heard disconnected peer event!');
 }
-async function handleScSignal() {
+async function handleScSignal({ description, candidate }) {
   console.log('Heard signal event!');
+  if (description) {
+    console.log('Received SDP Signal:', description);
+  } else if (candidate) {
+    console.log('Received ICE candidate:', candidate);
+  }
 }
 
 /**
